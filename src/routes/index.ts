8d4c3ce0471146f58b3f0a6ca07747a6811ef70f
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-14 13:05:53 +0700

    feat: ✨ register route api create post

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,22 @@
-import Elysia from "elysia";
-import { getPosts } from "../controllers/PostController";
+import {Elysia, t} from "elysia";
+import { getPosts,createPost } from "../controllers/PostController";
 
 const Routes = new Elysia({prefix: '/posts'})
 
     // get all data
-    .get('/', () => getPosts());
+    .get('/', () => getPosts())
+    // route create data post
+    .post('/', ({body}) => createPost(body as { title: string; content: string }), {
+        body: t.Object({
+            title: t.String({
+                minLength: 3,
+                maxLength: 100,
+            }),
+            content: t.String({
+                minLength: 3,
+                maxLength: 1000,
+            }),
+        })
+    })
 
 export default Routes;
