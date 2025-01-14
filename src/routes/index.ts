b2c90bcbfe2ef56a6bd928c325da255f0d3a0d17
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-14 16:42:57 +0700

    feat: ✨ create route update post

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import {Elysia, t} from "elysia";
-import { getPosts,createPost, getPostById } from "../controllers/PostController";
+import { getPosts, createPost, getPostById, updatePost } from "../controllers/PostController";
 
 const Routes = new Elysia({prefix: '/posts'})
 
@@ -20,5 +20,18 @@ const Routes = new Elysia({prefix: '/posts'})
     })
     // get data by id
     .get('/:id', ({params: { id }}) => getPostById(id))
-
+    // update post
+    .patch('/:id', ({params: {id}, body}) => updatePost(id, body as {title?: string; content?: string}), {
+        body: t.Object({
+            title: t.String({
+                minLength: 3,
+                maxLength: 100,
+            }),
+            content: t.String({
+                minLength: 3,
+                maxLength: 1000,
+            })
+        })
+    })
+    
 export default Routes;
