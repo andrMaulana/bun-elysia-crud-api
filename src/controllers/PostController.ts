e7a1240bbaa5d1cd423798d56518832a04ec9c8e
AndrMaulana <andryymaulana2000@gmail.com>
2025-01-14 13:05:38 +0700

    feat: ✨ create method create data posts

--- src/controllers/PostController.ts
+++ src/controllers/PostController.ts
@@ -18,3 +18,32 @@ export async function getPosts(){
         console.error(`Error getting posts: ${e}`);
     }
 }
+
+/**
+ * Creating a post
+ */
+
+export async function createPost(options: {title: string, content: string}) {
+    try{
+
+        // get title and content
+        const {title, content} = options;
+
+        // create data post
+        const post = await prisma.post.create({
+            data: {
+                title: title,
+                content: content,
+            }
+        });
+
+        // return response
+        return {
+            success: true,
+            message: "Post Created Successfully!",
+            data: post,
+        }
+    }catch(e: unknown) {
+        console.error(`Error creating post: ${e}`);
+    }
+}
